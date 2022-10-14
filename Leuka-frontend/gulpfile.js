ce4890d9ea94b99324f5440c6d831d1cc10cd83e
Dimitrije Zarkovic <dimitrije.zarkovic@vegait.rs>
2022-10-15 00:11:12 +0200

    Live reload added to gulp watch

--- Leuka-frontend/gulpfile.js
+++ Leuka-frontend/gulpfile.js
@@ -1,28 +1,49 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
 const concat = require("gulp-concat");
-const cleanCSS = require("gulp-clean-css");
+const sass = require("gulp-sass")(require("sass"));
 const del = require("del");
+const browserSync = require("browser-sync").create();
 
 const { src, dest } = gulp;
 
 gulp.task("build", (done) => {
-  gulp.series("clean", "pug")(done);
+  gulp.series(
+    "clean",
+    "scss",
+    "pug"
+  )(() => {
+    browserSync.reload();
+    done();
+  });
 });
 
 gulp.task("pug", () => src("./src/*.pug").pipe(pug()).pipe(dest("./dist")));
 
-gulp.task("css", () =>
-  src("./**/*.css")
-    .pipe(cleanCSS())
+gulp.task("scss", () =>
+  src([ "./assets/styles/index.scss", "./partials/**/*.scss"])
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(concat("index.css"))
-    .pipe(dest("./dist"))
+    .pipe(gulp.dest("./dist"))
 );
 
 gulp.task("watch", () => {
-  gulp.watch("partials/*.html", gulp.series("pug"));
-  gulp.watch("src/*.pug", gulp.series("pug"));
-  gulp.watch("partials/*.css", gulp.series("css"));
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+
+  gulp.watch(
+    [
+      "partials/*.scss",
+      "partials/**/*.scss",
+      "partials/**/*.html",
+      "src/*.pug",
+      "assets/styles/*.scss"
+    ],
+    gulp.series("build")
+  );
 });
 
-gulp.task("clean", () => del("./dist"));
+gulp.task("clean", () => del("./dist/*"));
