e2041048948f146fb9ea0947e8c6c86af0060991
j.vucetic <j.vucetic@vegait.rs>
2022-10-15 19:14:06 +0200

    Added gallery rendering

--- Leuka/Leuka.Core/ViewModels/Shared/GalleryViewModel.cs
+++ Leuka/Leuka.Core/ViewModels/Shared/GalleryViewModel.cs
@@ -8,13 +8,20 @@ namespace Leuka.Core.ViewModels.Shared
     {
         public GalleryViewModel(Gallery gallery)
         {
-            Images = gallery.Images.Select(x => new ImageViewModel(x));
-            NumberOfImages = Images.Count();
+            var images = gallery.Images.Select(x => new ImageViewModel(x)).ToList();
+
+            GalleryRows = images.Select((s, i) => new { Value = s, Index = i })
+                     .GroupBy(item => item.Index / 3, item => item.Value)
+                     .Cast<IEnumerable<ImageViewModel>>()
+                     .Select(x => new GalleryRowViewModel(x.ToList()))
+                     .ToList();
+
+            //NumberOfImages = Images.Count;
         }
 
         public int NumberOfImages { get; }
 
-        public IEnumerable<ImageViewModel> Images { get; }
+        public List<GalleryRowViewModel> GalleryRows { get; }
 
         public string PartialViewPath => "~/Views/Partials/NestedContent/_Gallery.cshtml";
     }
