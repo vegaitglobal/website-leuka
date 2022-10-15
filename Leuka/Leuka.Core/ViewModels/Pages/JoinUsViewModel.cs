48141ffc0674f3ae9d77caa82fa59cc4f7fd7b0f
Srdjan Besic <s.besic@vegait.rs>
2022-10-15 21:52:09 +0200

    First commit

--- Leuka/Leuka.Core/ViewModels/Pages/JoinUsViewModel.cs
+++ Leuka/Leuka.Core/ViewModels/Pages/JoinUsViewModel.cs
@@ -1,14 +1,29 @@
-﻿using Leuka.Core.Contexts;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Leuka.Core.Contexts;
+using Leuka.Core.ViewModels.Shared;
 using Leuka.Models.Generated;
 
 namespace Leuka.Core.ViewModels.Pages
 {
     public class JoinUsViewModel : PageViewModel
     {
-        public string Title { get; set; }
-        public JoinUsViewModel(IPageContext<JoinUs> context) : base(context)
+        public JoinUsViewModel(IPageContext<JoinUs> context)
+            : base(context)
         {
-            Title = context.Page.Title;
+            Components = new List<IContentViewModel>();
+
+            IEnumerable<Gallery> gallery = context.Page.Gallery;
+
+            if (gallery == null)
+            {
+                return;
+            }
+
+            IEnumerable<GalleryViewModel> galleryModels = gallery.Select(x => new GalleryViewModel(x));
+            Components.AddRange(galleryModels);
         }
+
+        public List<IContentViewModel> Components { get; set; }
     }
 }
