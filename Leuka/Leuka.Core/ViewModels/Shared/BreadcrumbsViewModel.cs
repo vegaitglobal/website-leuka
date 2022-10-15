365bed793e4a004d3f0331708157df7cd028762d
m-gula <m.gula@vegaitsourcing.rs>
2022-10-15 23:30:32 +0200

    feat: blog Page

---
+++ Leuka/Leuka.Core/ViewModels/Shared/BreadcrumbsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Leuka.Core.ViewModels.Shared;
+using System.Collections.Generic;
+
+namespace Leuka.Core.ViewModels.Shared
+{
+    public class BreadcrumbsViewModel : IContentViewModel
+    {
+        public BreadcrumbsViewModel()
+        {
+            Chain = new List<ButtonViewModel>();
+        }
+        public IList<ButtonViewModel> Chain { get; set; }
+        public string PartialViewPath => "~/Views/Partials/NestedContent/_BreadcrumbsViewModel.cshtml";
+    }
+}
