270b20c806820ba8e5b58490dc297744f00a5f00
Petar Kovacevic <p.kovacevic@vegait.rs>
2022-10-15 11:43:03 +0200

    - Create AboutUs page

---
+++ Leuka/Leuka.Core/Controllers/RenderMvc/AboutUsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Leuka.Core.Controllers.RenderMvc;
+using Leuka.Core.ViewModels.Pages;
+using Leuka.Models.Generated;
+
+namespace Leuka.Core.Controllers
+{
+    public class AboutUsController : BasePageController<AboutUs>
+    {
+        public ActionResult Index(AboutUs model) 
+            => CurrentTemplate(new AboutUsViewModel(CreatePageContext(model)));
+    }
+}
