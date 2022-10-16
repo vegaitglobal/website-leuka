315578cc39b94403893baf7d3e1bb7c6c4821048
darko.petrovic <d.petrovic@xximo.com>
2022-10-16 16:56:07 +0200

    fix error404 page

--- Leuka/Leuka.Core/Controllers/RenderMvc/Error404Controller.cs
+++ Leuka/Leuka.Core/Controllers/RenderMvc/Error404Controller.cs
@@ -14,7 +14,7 @@ namespace Leuka.Core.Controllers.RenderMvc
 			Response.TrySkipIisCustomErrors = true;
 
 			// CurrentTemplate won't work here as this controller may be hit from custom made route
-			return default; //return View(new Error404ViewModel(CreatePageContext(model)));
+			return View(new Error404ViewModel(CreatePageContext(model)));
 		}
 	}
 }
