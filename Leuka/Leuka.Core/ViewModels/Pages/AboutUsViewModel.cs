e195113ddadab69f87a636358a34419132ba3e10
Petar Kovacevic <p.kovacevic@vegait.rs>
2022-10-16 17:41:53 +0200

    - Enrich About us

--- Leuka/Leuka.Core/ViewModels/Pages/AboutUsViewModel.cs
+++ Leuka/Leuka.Core/ViewModels/Pages/AboutUsViewModel.cs
@@ -12,8 +12,14 @@ namespace Leuka.Core.ViewModels.Pages
         public AboutUsViewModel(IPageContext<Generated.AboutUs> context) : base(context)
         {
             ContactUsModals = context.Page.ContactUs.Select(x => new ContactusViewModel(x));
+            Buttons = context.Page.Buttons.Select(x => new ButtonViewModel(x));
+            Sponsors = context.Page.Sponsors.Select(x => new SponsorsViewModel(x));
+            HighlightedArticles = context.Page.HighlightedArticles.Select(x => new HighlightedArticleViewModel(x));
         }
 
         public IEnumerable<ContactusViewModel> ContactUsModals { get; }
+        public IEnumerable<ButtonViewModel> Buttons { get; }
+        public IEnumerable<SponsorsViewModel> Sponsors { get; }
+        public IEnumerable<HighlightedArticleViewModel> HighlightedArticles { get; }
     }
 }
