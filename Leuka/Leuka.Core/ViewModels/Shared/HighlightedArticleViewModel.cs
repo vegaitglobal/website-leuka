3879552f5e3280ee14ee40d35f64161dfdd1f601
stefan.radanovic <stefan.radanovic@vegait.rs>
2022-10-16 14:08:36 +0200

    added HighlightedArticle component

---
+++ Leuka/Leuka.Core/ViewModels/Shared/HighlightedArticleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Umbraco.Web;
+using Umbraco.Web.Models;
+using Leuka.Models.Generated;
+using System.Linq;
+
+namespace Leuka.Core.ViewModels.Shared
+{
+	public class HighlightedArticleViewModel : IContentViewModel
+    {
+		public HighlightedArticleViewModel(HighlightedArticle highlightedArticle)
+		{
+            HighlightTitle = highlightedArticle.HighlightTitle;
+            Category = highlightedArticle.Category;
+            Title = highlightedArticle.Title;
+            Description = highlightedArticle.Description;
+            Image = new ImageViewModel(highlightedArticle.Image);
+
+            if (highlightedArticle.LinkButton.Any())
+            {
+                Button = new ButtonViewModel(highlightedArticle.LinkButton.First());
+            }
+        }
+
+
+		public string HighlightTitle { get; }
+		public string Category { get; }
+		public string Title { get; }
+        public string Description { get; }
+        public ImageViewModel Image { get; }
+        public ButtonViewModel Button { get; }
+
+        public string PartialViewPath => "~/Views/Partials/NestedContent/_HighlightedArticle.cshtml";
+    }
+}
