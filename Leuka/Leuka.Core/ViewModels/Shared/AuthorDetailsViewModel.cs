d83339c4dbfb43fb6d61f867356f44d2e5d0685a
m-gula <m.gula@vegaitsourcing.rs>
2022-10-16 16:40:02 +0200

    feat: article css

--- Leuka/Leuka.Core/ViewModels/Shared/AuthorDetailsViewModel.cs
+++ Leuka/Leuka.Core/ViewModels/Shared/AuthorDetailsViewModel.cs
@@ -10,12 +10,20 @@ namespace Leuka.Core.ViewModels.Shared
             PublishDate = authorDetails.PublishDate.ToString("dd.MM.yyyy.");
             Description = authorDetails.Description;
             Image = new ImageViewModel(authorDetails.Image);
+            Facebook = new LinkViewModel(authorDetails.FacebookUrl);
+            Twitter = new LinkViewModel(authorDetails.TwitterUrl);
+            WhatsApp = authorDetails.WhatsAppInfo;
+            Google = new LinkViewModel(authorDetails.GoogleUrl);
         }
 
         public string FullName { get; }
         public string PublishDate { get; }
         public string Description { get; }
+        public string WhatsApp { get; }
         public ImageViewModel Image { get; }
+        public LinkViewModel Facebook { get; }
+        public LinkViewModel Twitter { get; }
+        public LinkViewModel Google { get; }
 
         public string PartialViewPath => "~/Views/Partials/NestedContent/_AuthorDetailsViewModel.cshtml";
 
